0339f11a3ef75bb77ab4fee4cca5b6d888d7c3c7
jan ancajas <jansquared@gmail.com>
2015-04-10 16:52:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert('hello world222222');
+var img = document.createElement("img");
+
+img.src = "https://d1lads9g6hso28.cloudfront.net/uploads/banner/image/41/320x50.gif";
+
+document.body.appendChild(img);
